8196fd040258e086348c2fdd462e53eeae109a00
nitinkumarmaurya890 <nitinkumarmaruya890@gmail.com>
2025-05-26 18:13:18 +0530

    add user authentication with login, profile retrieval, and logout functionality; implement cookie handling and token blacklisting

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,10 +1,11 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
+const blacklistTokenSchema = require("../models/blacklistToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
-//   console.log("BODY:", req.body);
+  //   console.log("BODY:", req.body);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
@@ -23,4 +24,44 @@ module.exports.registerUser = async (req, res, next) => {
   const token = user.generateAuthToken();
 
   res.status(201).json({ token, user });
+};
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(401).json({ message: "Inavlid email or password" });
+  }
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const token = user.generateAuthToken();
+
+  res.cookie("token", token);
+
+  res.status(200).json({ token, user });
+};
+
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie("token");
+  const token =
+    req.cookies.token || req.headers.generateAuthToken.split(" ")[1];
+
+  await blacklistTokenSchema.create({ token });
+
+  res.status(200).json({ message: "logged out" });
 };
